f9afafb2c29c8062f66c5a434ad99094de7a17d2
Jonathan <jonathan.kopp@t-online.de>
2023-07-13 20:39:42 +0200

    icp will now return the pose as rotation and translation matrix

--- src/ICP.cpp
+++ src/ICP.cpp
@@ -1,122 +1,131 @@
 #include "ICP.h"
 
-void gs::icp(std::vector<Point*> &dynamicPointCloud, std::vector<Point*> &staticPointCloud)
-{
-	float rotationMatrix[9];
-	float translation[3];
+#include <iostream>
+#include <utility>
 
-	std::vector<Point*> staticPointCloudCopy;
-	
-	gs::Point dynamicMid(0.0,0.0,0.0);
-	gs::Point staticMid(0.0,0.0,0.0);
+std::pair<std::vector<float>, std::vector<float>> gs::icp(std::vector<Point*>& dynamicPointCloud,
+                                                          std::vector<Point*>& staticPointCloud) {
+    float rotationMatrix[9];
+    float translation[3];
 
-	// copy the static point cloud
-	for (int i = 0; i < staticPointCloud.size(); i++)
-	{
-		Point* pCopy = new Point(staticPointCloud[i]->pos[0], staticPointCloud[i]->pos[1], staticPointCloud[i]->pos[2]);
-		staticPointCloudCopy.push_back(pCopy);
-	}
+    std::vector<Point*> staticPointCloudCopy;
 
-	// create the kd tree
-	KdTree* tree = new KdTree(staticPointCloudCopy);
-	size_t numDynamicPoints = dynamicPointCloud.size();
+    gs::Point dynamicMid(0.0, 0.0, 0.0);
+    gs::Point staticMid(0.0, 0.0, 0.0);
 
-	computeCloudMean(staticPointCloud, &staticMid);
-	computeCloudMean(dynamicPointCloud, &dynamicMid);
+    // copy the static point cloud
+    for (int i = 0; i < staticPointCloud.size(); i++) {
+        Point* pCopy = new Point(staticPointCloud[i]->pos[0], staticPointCloud[i]->pos[1], staticPointCloud[i]->pos[2]);
+        staticPointCloudCopy.push_back(pCopy);
+    }
 
-	// initialize the translation vector
-	clearTranslation(translation);
+    // create the kd tree
+    KdTree* tree = new KdTree(staticPointCloudCopy);
+    size_t numDynamicPoints = dynamicPointCloud.size();
 
-	// initialize the rotation matrix
-	clearRotation(rotationMatrix);
+    computeCloudMean(staticPointCloud, &staticMid);
+    computeCloudMean(dynamicPointCloud, &dynamicMid);
 
-	const int maxIterations = 400;
-	const int numRandomSamples = 400;
-	const float eps = 1e-8;
-	gs::Point p;
-	gs::Point x;
+    // initialize the translation vector
+    clearTranslation(translation);
 
-	float cost = 1.0;
-	std::srand(std::time(0));
+    // initialize the rotation matrix
+    clearRotation(rotationMatrix);
 
-	gs::Point qd;
-	gs::Point qs;
+    const int maxIterations = 400;
+    const int numRandomSamples = 400;
+    const float eps = 1e-8;
+    gs::Point p;
+    gs::Point x;
 
-	float U[9];
-	float w[9];
-	float sigma[3];
-	float V[9];
+    float cost = 1.0;
+    std::srand(std::time(0));
 
-	float** uSvd = new float*[3];
-	float** vSvd = new float*[3];
-	uSvd[0] = new float[3];
-	uSvd[1] = new float[3];
-	uSvd[2] = new float[3];
+    gs::Point qd;
+    gs::Point qs;
 
-	vSvd[0] = new float[3];
-	vSvd[1] = new float[3];
-	vSvd[2] = new float[3];
+    float U[9];
+    float w[9];
+    float sigma[3];
+    float V[9];
 
-	for (int iter = 0; iter < maxIterations && abs(cost) > eps; iter++)
-	{
-		cost = 0.0;
-		
-		//clearRotation(rotationMatrix);
-		clearMatrix(U);
-		clearMatrix(V);
-		clearMatrix(w);
-		computeCloudMean(dynamicPointCloud, &dynamicMid);
+    float** uSvd = new float*[3];
+    float** vSvd = new float*[3];
+    uSvd[0] = new float[3];
+    uSvd[1] = new float[3];
+    uSvd[2] = new float[3];
 
-		for (int i = 0; i < numRandomSamples; i++)
-		{
-			int randSample = std::rand() % dynamicPointCloud.size();
-			// sample the dynamic point cloud
-			p = *dynamicPointCloud[randSample];
+    vSvd[0] = new float[3];
+    vSvd[1] = new float[3];
+    vSvd[2] = new float[3];
 
-			// get the closest point in the static point cloud
-			tree->search(&p, &x);
+    for (int iter = 0; iter < maxIterations && abs(cost) > eps; iter++) {
+        cost = 0.0;
 
-			qd = p - dynamicMid;
-			qs = x - staticMid;
+        // clearRotation(rotationMatrix);
+        clearMatrix(U);
+        clearMatrix(V);
+        clearMatrix(w);
+        computeCloudMean(dynamicPointCloud, &dynamicMid);
 
-			outerProduct(&qs, &qd, w);
-			addMatrix(w, U, U);
+        for (int i = 0; i < numRandomSamples; i++) {
+            int randSample = std::rand() % dynamicPointCloud.size();
+            // sample the dynamic point cloud
+            p = *dynamicPointCloud[randSample];
 
-			cost += error(&x, &p, rotationMatrix, translation);
-		}
+            // get the closest point in the static point cloud
+            tree->search(&p, &x);
 
-		copyMatToUV(U, uSvd);
-		dsvd(uSvd, 3, 3, sigma, vSvd);
-		copyUVtoMat(uSvd, U);
-		copyUVtoMat(vSvd, V);
+            qd = p - dynamicMid;
+            qs = x - staticMid;
 
-		transpose(V);
-		matrixMult(U, V, rotationMatrix);
+            outerProduct(&qs, &qd, w);
+            addMatrix(w, U, U);
 
-		gs::Point t(0.0, 0.0, 0.0);
-		rotate(&dynamicMid, rotationMatrix, &t);
-		translation[0] = staticMid.pos[0] - t.pos[0];
-		translation[1] = staticMid.pos[1] - t.pos[1];
-		translation[2] = staticMid.pos[2] - t.pos[2];
+            cost += error(&x, &p, rotationMatrix, translation);
+        }
 
-		//update the point cloud
-		for (int i = 0; i < dynamicPointCloud.size(); i++)
-		{
-			rotate(dynamicPointCloud[i], rotationMatrix, &p);
-			translate(&p, translation, dynamicPointCloud[i]);
-		}
-	}
+        copyMatToUV(U, uSvd);
+        dsvd(uSvd, 3, 3, sigma, vSvd);
+        copyUVtoMat(uSvd, U);
+        copyUVtoMat(vSvd, V);
 
-	staticPointCloudCopy.clear();
-	delete tree;
-	
-	delete[] uSvd[0];
-	delete[] uSvd[1];
-	delete[] uSvd[2];
-	delete[] uSvd;
+        transpose(V);
+        matrixMult(U, V, rotationMatrix);
 
-	delete[] vSvd[0];
-	delete[] vSvd[1];
-	delete[] vSvd[2];
-	delete[] vSvd;
+        gs::Point t(0.0, 0.0, 0.0);
+        rotate(&dynamicMid, rotationMatrix, &t);
+        translation[0] = staticMid.pos[0] - t.pos[0];
+        translation[1] = staticMid.pos[1] - t.pos[1];
+        translation[2] = staticMid.pos[2] - t.pos[2];
+
+        // update the point cloud
+        for (int i = 0; i < dynamicPointCloud.size(); i++) {
+            rotate(dynamicPointCloud[i], rotationMatrix, &p);
+            translate(&p, translation, dynamicPointCloud[i]);
+        }
+    }
+
+    staticPointCloudCopy.clear();
+    delete tree;
+
+    delete[] uSvd[0];
+    delete[] uSvd[1];
+    delete[] uSvd[2];
+    delete[] uSvd;
+
+    delete[] vSvd[0];
+    delete[] vSvd[1];
+    delete[] vSvd[2];
+    delete[] vSvd;
+
+    std::vector<float> rot, trans;
+    for (auto r : rotationMatrix) {
+        rot.push_back(r);
+    }
+    for (auto t : translation) {
+        trans.push_back(t);
+    }
+
+    return std::make_pair(rot, trans);
 }
